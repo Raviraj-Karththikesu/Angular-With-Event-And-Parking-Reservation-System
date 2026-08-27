baaa72a7249a225847b050c7b1581fac377c73e5
CodeEver-CEO <raviraj20517@gmail.com>
2026-08-27 20:35:13 +0530

    chore: create initial ASP.NET Core Web API

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Event_and_parking_reservation_system
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
