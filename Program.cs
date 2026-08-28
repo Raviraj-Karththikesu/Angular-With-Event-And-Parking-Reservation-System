3905a24fa5fd82a028e379f2114523a40499f9c4
CodeEver-CEO <raviraj20517@gmail.com>
2026-08-28 10:47:44 +0530

    configure database connection

--- Program.cs
+++ Program.cs
@@ -1,36 +1,38 @@
+using Event_and_parking_reservation_system.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace Event_and_parking_reservation_system
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Controllers
+builder.Services.AddControllers();
+
+// Add Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register AppDbContext with SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    string? connectionString =
+        builder.Configuration.GetConnectionString(
+            "DefaultConnection");
 
-            // Add services to the container.
+    options.UseSqlServer(connectionString);
+});
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Enable Swagger only during development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
