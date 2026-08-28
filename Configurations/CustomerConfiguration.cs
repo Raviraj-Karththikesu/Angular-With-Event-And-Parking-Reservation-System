7120a3cd585770878a45786cdb06ce4546f33fd5
CodeEver-CEO <raviraj20517@gmail.com>
2026-08-28 10:21:13 +0530

    configure customer venue category and event entities

---
+++ Configurations/CustomerConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using Event_and_parking_reservation_system.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Event_and_parking_reservation_system.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.ToTable("Customers");
+
+        builder.HasKey(customer => customer.Id);
+
+        builder.Property(customer => customer.FullName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(customer => customer.Email)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.HasIndex(customer => customer.Email)
+            .IsUnique();
+
+        builder.Property(customer => customer.PhoneNumber)
+            .HasMaxLength(20);
+
+        builder.Property(customer => customer.PasswordHash)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(customer => customer.Role)
+            .HasConversion<string>()
+            .HasMaxLength(30);
+
+        builder.Property(customer => customer.Status)
+            .HasConversion<string>()
+            .HasMaxLength(30);
+
+        builder.Property(customer => customer.EmailVerificationTokenHash)
+            .HasMaxLength(500);
+
+        builder.Property(customer => customer.PasswordResetTokenHash)
+            .HasMaxLength(500);
+
+        builder.Property(customer => customer.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()");
+    }
+}
---
+++ Configurations/VenueConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Event_and_parking_reservation_system.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Event_and_parking_reservation_system.Configurations;
+
+public class VenueConfiguration : IEntityTypeConfiguration<Venue>
+{
+    public void Configure(EntityTypeBuilder<Venue> builder)
+    {
+        builder.ToTable(
+            "Venues",
+            table => table.HasCheckConstraint(
+                "CK_Venues_TotalCapacity",
+                "[TotalCapacity] > 0"));
+
+        builder.HasKey(venue => venue.Id);
+
+        builder.Property(venue => venue.Name)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(venue => venue.Address)
+            .IsRequired()
+            .HasMaxLength(300);
+
+        builder.Property(venue => venue.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()");
+
+        builder.HasIndex(venue => new
+        {
+            venue.Name,
+            venue.Address
+        }).IsUnique();
+    }
+}
