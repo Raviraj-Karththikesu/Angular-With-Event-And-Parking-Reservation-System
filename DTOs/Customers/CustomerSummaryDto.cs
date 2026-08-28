5907d240063b5eff2386d95c926ee53d032731c3
CodeEver-CEO <raviraj20517@gmail.com>
2026-08-28 11:42:13 +0530

    add customer DTO contracts

--- DTOs/Customers/CustomerSummaryDto.cs
+++ DTOs/Customers/CustomerSummaryDto.cs
@@ -1,6 +1,18 @@
-﻿namespace Event_and_parking_reservation_system.DTOs.Customers
+﻿namespace Event_and_parking_reservation_system.DTOs.Customers;
+
+public class CustomerSummaryDto
 {
-    public class CustomerSummaryDto
-    {
-    }
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Status { get; set; } = string.Empty;
+
+    public bool EmailVerified { get; set; }
+
+    public int TotalBookings { get; set; }
+
+    public int UpcomingBookings { get; set; }
 }
--- DTOs/Customers/UpdateCustomerDto.cs
+++ DTOs/Customers/UpdateCustomerDto.cs
@@ -1,6 +1,14 @@
-﻿namespace Event_and_parking_reservation_system.DTOs.Customers
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Event_and_parking_reservation_system.DTOs.Customers;
+
+public class UpdateCustomerDto
 {
-    public class UpdateCustomerDto
-    {
-    }
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string FullName { get; set; } = string.Empty;
+
+    [Phone]
+    [StringLength(20)]
+    public string? PhoneNumber { get; set; }
 }
