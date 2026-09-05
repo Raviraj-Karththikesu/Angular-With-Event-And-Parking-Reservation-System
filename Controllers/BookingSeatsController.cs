df7b182f76cef386f0efad66f2c437d26dad46ae
Shayanthan <ssayanthan506@gmail.com>
2026-09-05 15:41:34 +0530

    Implement seat booking and double-booking prevention

---
+++ Controllers/BookingSeatsController.cs
@@ -0,0 +1,46 @@
+﻿using Event_and_parking_reservation_system.DTOs.BookingSeats;
+using Event_and_parking_reservation_system.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_and_parking_reservation_system.Controllers;
+
+[ApiController]
+[Route("api/bookings/{bookingId:int}/seats")]
+[Authorize]
+public class BookingSeatsController : ControllerBase
+{
+    private readonly IBookingSeatService _bookingSeatService;
+
+    public BookingSeatsController(
+        IBookingSeatService bookingSeatService)
+    {
+        _bookingSeatService = bookingSeatService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<BookingSeatResponseDto>>> AddSeats(
+        int bookingId,
+        [FromBody] AddBookingSeatsDto dto)
+    {
+        var result =
+            await _bookingSeatService.AddSeatsToBookingAsync(
+                bookingId,
+                dto);
+
+        return StatusCode(
+            StatusCodes.Status201Created,
+            result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<BookingSeatResponseDto>>> GetSeats(
+        int bookingId)
+    {
+        var result =
+            await _bookingSeatService.GetBookingSeatsAsync(
+                bookingId);
+
+        return Ok(result);
+    }
+}
---
+++ Interfaces/Repositories/IBookingSeatRepository.cs
@@ -0,0 +1,14 @@
+﻿using Event_and_parking_reservation_system.Models;
+
+namespace Event_and_parking_reservation_system.Interfaces.Repositories;
+
+public interface IBookingSeatRepository
+{
+    Task<List<BookingSeat>> GetByBookingIdAsync(int bookingId);
+
+    Task<bool> HasActiveSeatAsync(int seatId);
+
+    Task AddRangeAsync(IEnumerable<BookingSeat> bookingSeats);
+
+    Task SaveChangesAsync();
+}
